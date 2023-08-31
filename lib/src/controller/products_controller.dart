b26f660fac807cdc6c3a9aefea05fcd940547238
Yves Atsuchi Takaoka <yvesat@gmail.com>
2023-08-31 01:55:16 -0300

    Criando product model e controller

--- lib/src/controller/products_controller.dart
+++ lib/src/controller/products_controller.dart
@@ -1,17 +1,31 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../model/product_model.dart';
 
 class ProductsController extends StateNotifier<AsyncValue<void>> {
   ProductsController() : super(const AsyncValue.data(null));
 
+  final db = FirebaseFirestore.instance;
+
   Product? getProduct(WidgetRef ref, String id) {
     return ref.read(productProvider.notifier).getProductById(id);
   }
 
-  List<Product>? getProductList(WidgetRef ref) {
-    return ref.read(productProvider.notifier).getProductList();
+  List<Product> getProductList(WidgetRef ref) {
+    return ref.watch(productProvider.notifier).filteredProductList();
+  }
+
+  // void filterProducts(WidgetRef ref, String query) {
+  //   ref.read(productProvider.notifier).filterProductList(query);
+  // }
+
+  Future<void> loadProducts(WidgetRef ref) async {
+    await db.collection("products").get().then((event) {
+      for (var product in event.docs) {
+        ref.read(productProvider.notifier).loadProduct(product.id, product.data());
+      }
+    });
   }
 }
 
-final productsController = StateNotifierProvider<ProductsController, AsyncValue<void>>((ref) => ProductsController());
+final productsControllerProvider = StateNotifierProvider<ProductsController, AsyncValue<void>>((ref) => ProductsController());
--- lib/src/model/product_model.dart
+++ lib/src/model/product_model.dart
@@ -1,37 +1,29 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class Product {
-  final String? id;
+  final String id;
   final String title;
-  final String description;
   final double price;
   final String imageUrl;
-  bool isFavorite;
 
   Product({
     required this.id,
     required this.title,
-    required this.description,
     required this.price,
     required this.imageUrl,
-    this.isFavorite = false,
   });
 
   Product copyWith({
     String? id,
     String? title,
-    String? description,
     double? price,
     String? imageUrl,
-    bool? isFavorite,
   }) {
     return Product(
       id: this.id,
       title: title ?? this.title,
-      description: description ?? this.description,
       price: price ?? this.price,
       imageUrl: imageUrl ?? this.imageUrl,
-      isFavorite: isFavorite ?? this.isFavorite,
     );
   }
 }
@@ -39,20 +31,29 @@ class Product {
 class ProductNotifier extends StateNotifier<List<Product>> {
   ProductNotifier() : super([]);
 
+  void loadProduct(String id, Map<String, dynamic> productData) {
+    final newProduct = Product(
+      id: id,
+      title: productData["title"],
+      price: productData["price"].toDouble(),
+      imageUrl: productData["imageUrl"],
+    );
+
+    state = [...state, newProduct];
+  }
+
   Product? getProductById(String id) {
     return state.firstWhere((e) => e.id == id);
   }
 
-  List<Product>? getProductList() {
+  List<Product> filteredProductList() {
     return state;
   }
 
-  void toggleFavorite(String id) {
-    state = [
-      for (final productState in state)
-        if (productState.id == id) productState.copyWith(isFavorite: productState.isFavorite = !productState.isFavorite) else productState,
-    ];
-  }
+  // void filterProductList(String query) {
+  //   final list = state.where((product) => product.title.toLowerCase().contains(query.toLowerCase())).toList();
+  //   _filteredProducts = list;
+  // }
 }
 
 final productProvider = StateNotifierProvider<ProductNotifier, List<Product>>((ref) => ProductNotifier());
