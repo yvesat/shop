1c0e6c6588308a10127fb5272f00faffde6f01d1
Yves Atsuchi Takaoka <yvesat@gmail.com>
2023-08-31 01:55:48 -0300

    Criando cart page e rota para cart page

--- lib/src/controller/routes_controller.dart
+++ lib/src/controller/routes_controller.dart
@@ -1,12 +1,14 @@
 import 'package:go_router/go_router.dart';
 
 import '../view/auth_page.dart';
+import '../view/cart_page.dart';
 import '../view/home_page.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(path: '/', builder: (context, state) => const AuthPage()),
     GoRoute(path: '/home', builder: (context, state) => const HomePage()),
+    GoRoute(path: '/cart', builder: (context, state) => const CartPage()),
   ],
   // redirect: (context, state) async {
   //   final isarService = IsarService();
