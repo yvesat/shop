e216f465e39f2bae93ea9e6858c84f9357af4cad
Yves Atsuchi Takaoka <yvesat@gmail.com>
2023-08-31 01:55:01 -0300

    Criando widgets auxiliares de produtos
    Grid de produto na home;
    Grid Item customizado para o produto;
    Search bar para buscar produtos

---
+++ lib/src/view/widgets/search_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomSearchBar extends StatelessWidget {
+  final TextEditingController controller;
+  final Function(String)? onChanged;
+
+  const CustomSearchBar({super.key, required this.controller, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      child: Stack(
+        alignment: Alignment.centerRight,
+        children: [
+          Expanded(
+            child: TextField(
+              onEditingComplete: () => Future.delayed(const Duration(), () => SystemChannels.textInput.invokeMethod('TextInput.hide')),
+              controller: controller,
+              onChanged: onChanged,
+              onTapOutside: (_) => SystemChannels.textInput.invokeMethod('TextInput.hide'),
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                isDense: true,
+                label: Text("Search a product", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black)),
+              ),
+            ),
+          ),
+          Positioned(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: InkWell(
+                onTap: () => controller.clear(),
+                child: const Icon(FontAwesomeIcons.x),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//TODO: LANGUAGE
